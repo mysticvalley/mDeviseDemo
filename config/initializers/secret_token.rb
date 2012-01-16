d2f11df5c58eb3ee664e78ec02d044041b1d2664
Rajan Maharjan <thamelstreet@gmail.com>
2012-01-16 10:45:49 +0545

    Added Devise Gem in Gem file and Bundle Install Done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MDeviseDemo::Application.config.secret_token = '4fa4e7f154c0fa4b66af7e034323f90c28850a4f69499b0a94c8d8967aaff38625347314075c0a78d5675ff41e998a129f4c8470db38dc56a310fb41f14859f2'
